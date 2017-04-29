942ebffde30b2cf2ddced6817dac81e8e122f714
Vinh Vu <anhvinhvu95@gmail.com>
2017-04-29 00:50:26 -0700

    Add Nav,Battle,Home components

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,24 +3,23 @@ var NavLink = require('react-router-dom').NavLink;
 
 function Nav(){
 	return (
-		<ul className='nav'>
-		  <li>
-		    <NavLink exact activeClassName='active' to ='/'>
-		      Home
-		    </NavLink>
-		  </li>
-		  <li>
-		    <NavLink activeClassName='active' to ='/battle'>
-		      Battle
-		    </NavLink>
-		  </li>
-		  <li>
-		  	<NavLink activeClassName='active' to ='/popular'>
-		      Popular
-		    </NavLink>
-		  </li>
-		</ul>
-
+	  <ul className='nav'>
+		<li>
+		  <NavLink exact activeClassName='active' to ='/'>
+		    Home
+		  </NavLink>
+		</li>
+		<li>
+		  <NavLink activeClassName='active' to ='/battle'>
+		    Battle
+		  </NavLink>
+		</li>
+		<li>
+		  <NavLink activeClassName='active' to ='/popular'>
+		    Popular
+		  </NavLink>
+		</li>
+	  </ul>
 	)
 }
 
